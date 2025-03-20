bc6c30a26eb275f1d2cd95f4a4afa9ffc04a5e6a
ArtFiorindo <alopesfiorindo@gmail.com>
2025-03-20 12:54:14 -0300

    Removendo o projeto antigo

--- Models/PacienteOcorrenciaViewModel.cs
+++
@@ -1,8 +0,0 @@
-namespace OnData.Models
-{
-    public class PacienteOcorrenciaViewModel
-    {
-        public Paciente Paciente { get; set; }
-        public Ocorrencia Ocorrencia { get; set; }
-    }
-}
